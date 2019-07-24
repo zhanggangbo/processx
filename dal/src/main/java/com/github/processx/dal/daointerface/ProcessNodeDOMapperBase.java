66e3d089de0b28d362dd9ec062e23019f39cc0a0
zhanggangbo <zhanggangbo@dingtalk.com>
2019-07-25 00:22:35 +0800

    项目初始

---
+++ dal/src/main/java/com/github/processx/dal/daointerface/ProcessNodeDOMapperBase.java
@@ -0,0 +1,13 @@
+/**
+ * Lingxi.com Inc.
+ * Copyright (c) 2018-2019 All Rights Reserved.
+ */
+package com.github.processx.dal.daointerface;
+
+/**
+ *
+ * @author zhanggangbo
+ * @version v 0.1 2019/7/24 23:23
+ */
+public interface ProcessNodeDOMapperBase {
+}
